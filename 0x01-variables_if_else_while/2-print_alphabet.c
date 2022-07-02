0495f8122f66e33696f2af879d6e4a156e8fe146
Binale <binalematumbai@gmail.com>
2022-07-02 23:48:14 +0000

    Create 2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Print the alphabet in lowercase followed by a new line
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char ch_counter = 'a';
+
+	while (ch_counter < 'z')
+	{
+		putchar(ch_counter);
+		ch_counter++;
+	}
+	putchar('\n');
+	return (0);
+}
