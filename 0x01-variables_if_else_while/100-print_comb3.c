23b097c450aeb14185224dec9fc7d2a3a12b1ad5
Binale <binalematumbai@gmail.com>
2022-07-04 23:55:57 +0000

    Create 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ *
+ * Description: 
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int num = 0, tens_digit, ones_digit;
+
+	while (num < 100)
+	{
+		tens_digit = num / 10;
+		ones_digit = num % 10;
+		if (tens_digit < ones_digit)
+		{
+			putchar(tens_digit + '0');
+			putchar(ones_digit + '0');
+			if (num < 89)
+			{
+				putchar(44);
+				putchar(32);
+			}
+		}
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
