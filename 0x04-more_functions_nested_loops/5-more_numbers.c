b36153fac84a2a8aee3a0fe7cf199582c4258a1f
Binale <binalematumbai@gmail.com>
2022-07-07 23:26:18 +0000

    Create 5-more_numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times the numbers,
+ * from 0 - 14, followed by a new line
+ */
+
+void more_numbers(void)
+{
+	int j = 0, k , l;
+
+	for (; j <= 10; j++)
+	{
+		k = 0;
+		for (; k <= 14; k++)
+		{
+			if (k > 9)
+			{
+				l = k / 10;
+				_putchar(l + '0');
+				l = k % 10;
+				_putchar(l + '0');
+			}
+			else
+			{
+				_putchar(k + '0');
+			}
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
