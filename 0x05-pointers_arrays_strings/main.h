7c15bad436e81814650f95188f941f4c5ccbfbd9
Matumbai Binale <binalematumbai@gmail.com>
2023-10-16 13:25:10 -0700

    Create 3-puts.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,10 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char *);
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 #include <stddef.h>
 int _strlen(char *s);
+void _puts(char *str);
 
 #endif
