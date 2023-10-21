9996bc9b0cad5645922ae418763df049b3445df5
Matumbai Binale <binalematumbai@gmail.com>
2023-10-21 08:27:13 -0700

    Null-pad 'dest' if 'src' is shorter than 'n'

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,25 +1,38 @@
 #include "main.h"
 
 /**
- * _strncpy - copies a string
- * @dest: the copied string
- * @src: the string to copy
- * @n: number of bytes to copy from src to dest
- * Return: char * - The copied string
+ * _strncpy - Copies at most 'n' characters from 'src'to 'dest'
+ * and null-pads 'dest' if 'n'is greater than the length of 'src'
+ * @dest: The destination string
+ * @src: The source string
+ * @n: The maximum number of characters to copy from 'src'
+ * Return: A pointer to the resulting string 'dest'
  */
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i, len_src;
 
+	/* Calculate the length of the source string 'src' */
 	for (len_src = 0; src[len_src] != '\0'; len_src++)
 		;
 
+	/* Copy at most 'n' characters from 'src'to 'dest' */
 	for (i = 0; i < n; i++)
 	{
 		dest[i] = src[i];
 	}
 
-	return (dest);
+	/* Null-pad 'dest' if 'n' is greater than the length of 'src' */
+	if (len_src < n)
+	{
+		while (len_src < n)
+		{
+			dest[len_src] = '\0';
+			len_src++;
+		}
+	}
+
+	return (dest); /* Return a pointer to the resulting string, dest */
 }
 
 
