662980a4e25cadc424f0375afe43f137c1b1c91f
Matumbai Binale <binalematumbai@gmail.com>
2023-10-26 13:03:24 -0700

    Edit descripton for args x, and y

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -2,8 +2,8 @@
 
 /**
  * _pow_recursion - raises x to the power of y
- * x: An integer, the base
- * y: An integer, the exponent
+ * @x: An integer, the base
+ * @y: An integer, the exponent
  * Return: Result of exponentiation
  */
 int _pow_recursion(int x, int y)
