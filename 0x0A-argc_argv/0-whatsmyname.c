53ef1177436157985d86785f678e71179799db95
Matumbai Binale <binalematumbai@gmail.com>
2023-10-29 12:33:43 -0700

    Create 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the name of the program
+ * @argc: Number of command line arguments
+ * @argc: An array of strings, which are the arguments
+ * Return: An integer
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+	exit(EXIT_SUCCESS);
+}
