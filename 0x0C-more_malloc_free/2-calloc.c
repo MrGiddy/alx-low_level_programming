b7c47b80897c8b3394269d8324a2b51b0bcf144e
Matumbai Binale <binalematumbai@gmail.com>
2023-11-03 11:15:45 -0700

    Comment 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -14,23 +14,30 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	char *ptr;
 	unsigned int mem_size, i;
 
+	/* Check for valid input */
 	if (nmemb == 0 || size == 0)
 	{
 		return (NULL);
 	}
 
+	/* Calculate the total memory size to be allocated */
 	mem_size = nmemb * size;
+
+	/* Allocate memory using malloc */
 	ptr = malloc(mem_size);
 
+	/* Check if memory allocation was successful */
 	if (ptr == NULL)
 	{
 		return (NULL);
 	}
 
+	/* Initiaze allocated memory to zero */
 	for (i = 0; i < mem_size; i++)
 	{
 		ptr[i] = 0;
 	}
 
+	/* Return a pointer to the allocated and zero-initialized memory block */
 	return (ptr);
 }
