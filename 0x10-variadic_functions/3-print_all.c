d8698f70622fde28d3992ec24c2f40c0f774bbca
Matumbai Binale <binalematumbai@gmail.com>
2023-11-11 00:19:57 -0800

    Create 3-print_all.c

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,76 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdio.h>
+
+/**
+ * _strlen - Computes the length of a string
+ * @str: The string to find length
+ * Return: An integer, which is the length of the string
+ */
+int _strlen(const char *str)
+{
+	int str_len;
+
+	for (str_len = 0; str[str_len] != '\0'; str_len++)
+		;
+
+	return (str_len);
+}
+
+/**
+ * print_all - A function that prints anything
+ * @format: A list of types of arguments passed to the function
+ * Return: Nothing
+ */
+void print_all(const char * const format, ...)
+{
+	va_list args;
+	int num_args, i;
+	char *str;
+
+	va_start(args, format);
+
+	num_args = _strlen(format);
+
+	i = 0;
+	while (i < num_args)
+	{
+		switch (format[i])
+		{
+			case 'c':
+				printf("%c", va_arg(args, int));
+				break;
+			case 'i':
+				printf("%d", va_arg(args, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(args, double));
+				break;
+			case 's':
+				str = va_arg(args, char *);
+				if (str == NULL)
+				{
+					printf("%s", "(nil)");
+					break;
+				}
+				else
+				{
+					printf("%s", str);
+					break;
+				}
+			default:
+				break;
+		}
+
+		if (i < num_args && (format[i] == 'c' || format[i] == 'i' || format[i] == 'f' || format[i] == 'f'))
+		{
+			printf(", ");
+		}
+
+		i++;
+	}
+	printf("\n");
+
+	va_end(args);
+}
