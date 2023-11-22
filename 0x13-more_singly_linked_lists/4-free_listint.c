1e69ffb16a76d757b32dd7a4facec143a3d4973d
Matumbai Binale <binalematumbai@gmail.com>
2023-11-22 09:14:20 -0800

    Fix errors in freeing memory, 4-free_listint.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -2,19 +2,27 @@
 
 /**
  * free_listint - A function that frees a type listint_t list
- * @head: A pointer to the head node of a listint_t 
+ * @head: A pointer to the head node of a listint_t
  * Return: Nothing
  */
 void free_listint(listint_t *head)
 {
-	listint_t *ptr;
+	listint_t *ptr, *temp;
 
+	/* Initialize ptr to the head of the list */
 	ptr = head;
 
+	/* Traverse the list and free each node */
 	while (ptr->next != NULL)
 	{
+		/* Save the reference to the next node */
+		temp = ptr->next;
+
+		/* Free the current node */
 		free(ptr);
-		ptr = ptr->next;
+
+		/* Move to the next node */
+		ptr = temp;
 	}
 	free(ptr);
 }
