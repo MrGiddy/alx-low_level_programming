275d6d0ecce63ec4d2a29c6acdf67ae594b0ee22
Matumbai Binale <binalematumbai@gmail.com>
2024-02-03 10:10:22 -0800

    (5-hash_table_print.c): Match format output for 5-main.c

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -13,8 +13,13 @@ void hash_table_print(const hash_table_t *ht)
 	hash_node_t *temp;
 	int comma;
 
+	if (ht == NULL)
+		return;
+
+	comma = 0;
+
 	printf("{");
-	for (i = 0; i < ht->size && ht; i++)
+	for (i = 0; i < ht->size; i++)
 	{
 		if (ht->array[i])
 		{
@@ -24,7 +29,7 @@ void hash_table_print(const hash_table_t *ht)
 			temp = ht->array[i];
 			while (temp != NULL)
 			{
-				printf("'%s: %s'", temp->key, temp->value);
+				printf("'%s': '%s'", temp->key, temp->value);
 
 				if (temp->next != NULL)
 					printf(", ");
